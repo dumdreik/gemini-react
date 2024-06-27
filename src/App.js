befb7d8c67fe1e113d65cc9b8dca82d7948a534c
e.semichastnov <e.semichastnov@web-hands.ru>
2024-06-27 17:38:45 +0300

    1

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Sidebar from "./components/Sidebar/Sidebar";
+import Main from "./components/Main/Main";
+
+function App(props) {
+	return (
+		<>
+			<Sidebar/>
+			<Main/>
+		</>
+	);
+}
+
+export default App;
